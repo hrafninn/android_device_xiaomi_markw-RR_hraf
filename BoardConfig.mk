00ae60defe70cf436c2b022c36bcd33e54d29621
hrafninn <herr.hrafninn@gmail.com>
2017-09-21 00:35:00 +0200

    prop/sensor tweaks
    Change-Id: I3c78cb0e7573caacfd0da5683dc99ae0e81123e1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -61,7 +61,7 @@ BOARD_RAMDISK_OFFSET := 0x01000000
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_CONFIG := markw_defconfig
 #TARGET_KERNEL_SOURCE := kernel/xiaomi/markw
-TARGET_KERNEL_SOURCE := kernel/xiaomi/markw_new/android_kernel_xiaomi_msm8953
+TARGET_KERNEL_SOURCE := kernel/xiaomi/markw_new
 
 USE_CLANG_PLATFORM_BUILD := true
 
