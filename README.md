411662ec7cc77913d9cd66649fdd0acf0619cea4
hrafninn <herr.hrafninn@gmail.com>
2017-10-16 22:34:12 +0200

    merged karev's changes
    Change-Id: I86677e8709713ef503da116b35127b23ae6c8d15

--- README.md
+++ README.md
@@ -14,10 +14,10 @@ Storage | 32 GB
 MicroSD | Up to 256 GB (Hybrid)
 Battery | 4100 mAh (non-removable)
 Dimensions | 141.3 x 69.6 x 8.9 mm
-Display | 1080 x 1920 pixels, 5.0" IPS
-Rear Camera  | 13.0 MP, Dual LED flash
-Front Camera | 5.0 MP
-FingerPrint | Yes
+Display | 1080 x 1920 pixels, 5.0" IPS (Tianma/EBBG)
+Rear Camera  | 13.0 MP, Dual LED flash (Samsung S5K3L8/OmniVision OV13853)
+Front Camera | 5.0 MP (OmniVision OV5670)
+FingerPrint | Yes (FPC/Goodix)
 Release Date | November 2016
 
 ![Xiaomi Redmi 4 Prime](http://cdn2.gsmarena.com/vv/pics/xiaomi/xiaomi-redmi-4-prime-2.jpg "Xiaomi Redmi 4 Prime")
--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -291,16 +291,14 @@ case "$target" in
                 #governor settings
                 echo 1 > /sys/devices/system/cpu/cpu0/online
                 echo "interactive" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-                echo "19000 1401600:39000" > /sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay
-                echo 85 > /sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load
+                echo "19000 1401600:29000" > /sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay
+                echo 95 > /sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load
                 echo 20000 > /sys/devices/system/cpu/cpufreq/interactive/timer_rate
                 echo 1401600 > /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq
                 echo 0 > /sys/devices/system/cpu/cpufreq/interactive/io_is_busy
                 echo "85 1401600:80" > /sys/devices/system/cpu/cpufreq/interactive/target_loads
-                echo 39000 > /sys/devices/system/cpu/cpufreq/interactive/min_sample_time
-                echo 40000 > /sys/devices/system/cpu/cpufreq/interactive/sampling_down_factor
-                echo 652800 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-
+                echo 29000 > /sys/devices/system/cpu/cpufreq/interactive/min_sample_time
+                
                 # re-enable thermal & BCL core_control now
                 echo 1 > /sys/module/msm_thermal/core_control/enabled
                 for mode in /sys/devices/soc.0/qcom,bcl.*/mode
